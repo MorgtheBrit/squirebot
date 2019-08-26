3b56bc09f4a0558e8004e994f1459575e584ab19
Morgan Humphreys <morganjhumphreys@gmail.com>
2019-08-26 16:09:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,23 +3,15 @@ import discord
 client = discord.Client()
 prefix = "!!"
 
+
 #Help functions for events
 
 def get_parameters(message):
-    args = message.content.slice(prefix.length).trim().split(/ +/g)
-    args.shift().lower()
+    args = message.content.split()
     return args
 
-def check_cat(catname):
-    if(discord.CategoryChannel(name=catname))
-        return(True)
-    else
-        return(False)
-
-def create_tempcat(catname):
-    tempcat = create_category_channel(name=catname,position=server.channels.array().length + 1)
-    return(tempcat.id)
-
+def clamp(n, minn, maxn):
+    return max(min(maxn, n), minn)
 
 #Events
 
@@ -29,30 +21,55 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+
+    #Protects from potential loops from the bot itself.
     if message.author == client.user:
         return
 
     #Help menu
     if message.content.lower().startswith(prefix + 'help'):
-        #initialize array to allow for simple adding of additional commands
-    	commands={}
-        commands['!!help']='Sends the user a help menu that outlines all functionality of the bot'
-    	commands['!!voice']='Create a temporary voice channel'
+        await show_help(message)
 
-    	msg = discord.Embed(title='Squire Bot', description="A simple Python bot by MorgtheBrit") #initialize the embeded object
-    	for command,description in commands.items(): #loop around adding commands to the embed object msg
-    	       msg.add_field(name=command,value=description)
-    	await message.channel.send(embed=msg) #print help menu to channel
+    #Create Temporary Voice Channel
+    if message.content.lower().startswith(prefix + 'voice'):
+        await create_voice(message)
 
 @client.event
-async def create_voice():
-    if message.content.lower().startswith(prefix + 'voice'):
-        if(check_cat("Temp Voice") == False)
-            create_tempcat("Temp Voice")
+async def show_help(message):
 
-        args[] = get_parameters(message)
-        assert (args.length = 2), "You did not provide adequate information please provide !!voice {name} {slots}"
-        vc = discord.VoiceChannel(name=args[0], user_limit=args[1])
+    commands={}
+    commands[prefix+"help"] = "Prints help menu that on how to use the bot"
+    commands[prefix+"voice {name} {slots}"] = "Create a Temporary Voice Channel"
 
+    msg = discord.Embed(title='Squire Bot',description="Python bot by MorgtheBrit")
+
+    #loop around adding commands to the embed object msg
+    for command,description in commands.items():
+        msg.add_field(name=command,value=description, inline=False)
+
+    #print help menu to channel
+    await message.channel.send(embed=msg)
+
+@client.event
+async def create_voice(message):
+
+    #Specify the category
+    server = message.author.guild
+    vccat = server.get_channel("Channelid")
+
+    #Extract parameters and create the voice channel
+    args = get_parameters(message)
+
+    try:
+        if(len(args) == 3):
+            args[2] = clamp(int(args[2]), 0, 99)
+
+            vc = await vccat.create_voice_channel(name=args[1], user_limit=args[2])
+        else:
+            msg = discord.Embed(title='Opps something went wrong!' ,description='Please provide !!voice {name} {slots}')
+            await message.channel.send(embed=msg)
+    except discord.errors.HTTPException:
+        msg = discord.Embed(title='Opps something went wrong!' ,description='Please ensure your name is between 1-99')
+        await message.channel.send(embed=msg)
 
 client.run('Token')
